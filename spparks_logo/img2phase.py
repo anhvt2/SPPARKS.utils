c1c7e173874e7453f7479ac3db827ed355c082ea
Anh Tran <anh.vt2@gmail.com>
2023-06-09 09:01:40 -0400

    add input and img2phase.py

--- spparks_logo/img2phase.py
+++ spparks_logo/img2phase.py
@@ -1,4 +1,12 @@
 
 import skimage.io as io
 import numpy as np
-d = io.imread('cropped.spparks.png', as_gray=True)
+d = io.imread('cropped.spparks.png', as_gray=True) # d.shape = (192, 1039)
+
+for i in range(d.shape[0]):
+	for j in range(d.shape[1]):
+		if d[i,j] < 1:
+			d[i,j] = 0
+
+io.imshow(d)
+plt.show()
